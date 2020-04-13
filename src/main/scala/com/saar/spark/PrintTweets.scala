9edb11b542c81daf035882c55b29a0eec7bd021e
saara <Saar.Avraham@nice.com>
2020-04-13 19:29:19 +0300

    .

--- src/main/scala/com/saar/spark/PrintTweets.scala
+++ src/main/scala/com/saar/spark/PrintTweets.scala
@@ -5,9 +5,7 @@ package com.saar.spark
 import java.sql.DriverManager
 
 import Utilities._
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.streaming.rabbitmq.RabbitMQUtils
-import org.apache.spark.streaming.twitter.TwitterUtils
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 
 /** Simple application to listen to a stream of Tweets and print them out */
@@ -22,30 +20,47 @@ object PrintTweets {
     // all CPU cores and one-second batches of data
     val ssc = new StreamingContext("local[*]", "PrintTweets", Seconds(1))
 
-    // Create a DStream from Twitter using our streaming context
-    val tweets = TwitterUtils.createStream(ssc, None)
+    val messages = RabbitMQUtils.createStream[String](ssc,
+      Map("queueName" -> "SaarNewQueue3",
+      "exchangeName" -> "tut.fanout",
+      "password" -> "nice",
+      "userName" -> "nice"
+    ));
 
-    tweets.foreachRDD{
+    messages.foreachRDD{
       rdd =>
-        rdd.foreachPartition { part =>
-          val conn= DriverManager.getConnection("jdbc:sqlserver://DESKTOP-S42L7FK\\MSSQLSERVER01;databaseName=d", "sa", "sa")
-          val batchInsert = conn.prepareStatement ("INSERT INTO dt (value) VALUES (?) ")
+        rdd.foreachPartition{
+          part =>
+            for (message <- part)
+                    {
+                      println("!@# " + message)
 
-          //        batchInsert.setString(1,"%s".format(rdd.id))
-          //        batchInsert.addBatch()
-          //        batchInsert.clearParameters()
-          for (row <- part)
-          {
-            batchInsert.setString(1,"%s".format(row.getText))
-            batchInsert.addBatch()
-            batchInsert.clearParameters()
-          }
+                    }
 
-          println("lines inserted: "+ batchInsert.executeBatch().sum)
         }
     }
+//
+//    tweets.foreachRDD{
+//      rdd =>
+//        rdd.foreachPartition { part =>
+//          val conn= DriverManager.getConnection("jdbc:sqlserver://DESKTOP-S42L7FK\\MSSQLSERVER01;databaseName=d", "sa", "sa")
+//          val batchInsert = conn.prepareStatement ("INSERT INTO dt (value) VALUES (?) ")
+//
+//          //        batchInsert.setString(1,"%s".format(rdd.id))
+//          //        batchInsert.addBatch()
+//          //        batchInsert.clearParameters()
+//          for (row <- part)
+//          {
+//            batchInsert.setString(1,"%s".format(row.getText))
+//            batchInsert.addBatch()
+//            batchInsert.clearParameters()
+//          }
+//
+//          println("lines inserted: "+ batchInsert.executeBatch().sum)
+//        }
+//    }
 
     ssc.start()
     ssc.awaitTermination()
-  }  
+  }
 }
