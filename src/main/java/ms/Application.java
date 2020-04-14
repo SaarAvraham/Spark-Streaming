bba52d70985f2e70aec0c18aac7ad216bb64fee3
saara <Saar.Avraham@nice.com>
2020-04-14 12:37:14 +0300

    .

--- src/main/java/ms/Application.java
+++ src/main/java/ms/Application.java
@@ -3,6 +3,7 @@ package ms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import scala.Tuple2;
 import scala.collection.Seq;
 
@@ -28,5 +29,27 @@ public class Application {
 ////            rabbitTemplate.convertAndSend(fanoutExchange.getName(), "", message);
 //        }
 //
+//    }
+
+//    @Bean
+//    public org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory() {
+//        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+//        connectionFactory.setPort(rabbitMQPort);
+//        connectionFactory.setHost(rabbitMQHostname);
+//        connectionFactory.setUsername(rabbitMQUser);
+//        connectionFactory.setPassword(rabbitMQPassword);
+////        connectionFactory.setRequestedHeartBeat(20);
+//        return connectionFactory;
+//    }
+//
+//    @Bean
+//    public RabbitTemplate rabbitTemplate(org.springframework.amqp.rabbit.connection.ConnectionFactory connectionFactory) {
+//        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+//        //The routing key is set to the name of the queue by the broker for the default exchange.
+////        template.setRoutingKey(rabbitmqQueueName);
+//        template.setExchange(rabbitMQExchange);
+//        //Where we will synchronously receive messages from
+////        template.setDefaultReceiveQueue(rabbitmqQueueName);
+//        return template;
 //    }
 }
